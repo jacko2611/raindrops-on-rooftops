f3bb64eab868ad20b406d35ff16334ff05a3fc59
jacko2611 <jacksonkent70@gmail.com>
2023-01-16 22:35:45 +1100

    added weather api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,17 @@ let weather = {
     "apiKey": "1740775d2c961c3655dcccb88a096ec1",
     fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q="
-             + city +
-            "&appid=1740775d2c961c3655dcccb88a096ec1"
-             + this.apiKey
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey
         )
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
+        const name = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed); 
 
     }
 }
