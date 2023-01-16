40b68bb15e315a5b91bf7c04ab8ee407da903a48
jacko2611 <jacksonkent70@gmail.com>
2023-01-16 18:24:48 +1100

    adding up to date code to develop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 let weather = {
-    "apiKey": "1740775d2c961c3655dcccb88a096ec1"
+    "apiKey": "1740775d2c961c3655dcccb88a096ec1",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city +
+            "&appid=1740775d2c961c3655dcccb88a096ec1"
+             + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+    displayWeather: function (data) {
+
+    }
 }
 
