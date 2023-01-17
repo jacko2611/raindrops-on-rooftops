145d6fede698a452933cbec1492832aff2249494
jacko2611 <jacksonkent70@gmail.com>
2023-01-17 22:56:17 +1100

    finished js functionality, app is complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let weather = {
-    "apiKey": "1740775d2c961c3655dcccb88a096ec1",
+    apiKey: "1740775d2c961c3655dcccb88a096ec1",
     fetchWeather: function (city) {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey
@@ -8,7 +8,8 @@ let weather = {
             .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
-        const name = data;
+        const { name } = data;
+        console.log(data);
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
@@ -16,14 +17,22 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        const tempInCelsius = temp - 273.15
+        document.querySelector(".temp").innerText = tempInCelsius.toFixed(0) + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
     }
 }
 function searchWeather() {
-    fetchWeather(document.querySelector(".search-bar").value);
+    weather.fetchWeather(document.querySelector(".search-bar").value);
 }
 document.querySelector(".search button").addEventListener("click", function(){
     searchWeather();
 });
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if (event.key == "Enter") {
+        searchWeather();
+    }
+})
